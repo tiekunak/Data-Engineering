088974267811583bedab7282594490479b2690b5
Bernard Tieku <76092596+tiekunak@users.noreply.github.com>
2022-09-29 11:26:24 -0400

    Update New_twitter_streaming.py

--- New_twitter_streaming.py
+++ New_twitter_streaming.py
@@ -1,7 +1,6 @@
-# http://adilmoujahid.com/posts/2014/07/twitter-analytics/
+# Bernard Tieku
 # If you're collecting tweets with very popular keywords (e.g., bigdata, iop, trump, applebee), use the following script.
 
-#from tweepy.streaming import StreamListener
 import tweepy
 
 # Saving data as
@@ -27,11 +26,11 @@ class StdOutListener(tweepy.Stream):
 if __name__ == '__main__':
 
     #This handles Twitter authetification and the connection to Twitter Streaming API
-    stream = StdOutListener('CWEwmsn9rZ0hAcw4yflugnFl8', '5HkhR51YmNcJnO7PHmol7iEOBNlMmMmNv5QIhLhm8g8KiclEqP', '878936672-KjFi2bgEyFDu6Wo9QT7fzcRQ2jfqxe4E1ZCVsdfY', 'CyTkbXDmX12pTTgoR8xaLQ7OdjelKxTbO182Nc1WtpGda')
+    stream = StdOutListener('consumer_key', 'consumer_secret', 'access_token', 'access_token_secret')
 
 while True:
 	try:
-		#stream = Stream(auth, l)
+		
 		#This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby', '#bigdata'
 		stream.filter(track=['GUDA', 'Akuffo Addo', 'kirani ayat'])
 	except: 
